545b5ae5ad2a2776d8996ff0b82964b605e2ced7
Gouthamve <cs14btech11014@iith.ac.in>
2015-05-27 18:09:16 +0530

    Change random string to 7 characters

--- models/Image.js
+++ models/Image.js
@@ -12,8 +12,7 @@ var Image = new keystone.List('Image');
 Image.add({
 	image: { type: Types.LocalFile, dest: 'public/images/',
 		filename: function(item, file){
-			console.log(file.extension)
-			return randomString(5) + '.' + file.extension
+			return randomString(7) + '.' + file.extension
 		},
 		format: function(item, file){
 			return '<img src="/images/'+file.filename+'" style="max-width: 300px">'
